594169ad572662ef7a0dbd584271219233707a9c
linkthehylian <andypurdy3@gmail.com>
2019-08-06 00:54:00 -0400

    Edited cs

---
+++ SplashScreen.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UNO;
+using UNO.GamePlay;
+
+public class SplashScreen : MonoBehaviour
+{
+	private int m_movieIndex;
+
+	public GameObject PS4ScreenPlane;
+
+	public GameObject X1ScreenPlane;
+
+	public string[] MovieList;
+
+	public GameObject m_epilepsyWarning;
+
+	private List<MovieTexture> m_movieList = new List<MovieTexture>();
+
+	private bool m_showVideo;
+
+	private void Start()
+	{
+		SimpleSingleton<UplayPC>.Instance.Attach();
+		initGamePlayLogic();
+		base.StartCoroutine(begin());
+		QualitySettings.vSyncCount = 0;
+	}
+
+	private IEnumerator begin()
+	{
+		yield return (object)new WaitForSeconds(0.05f);
+		m_epilepsyWarning.GetComponent<epliepsyController>().go(playMovies);
+		string url = "Menu/3D_Assets/MainMenu/SpawnManager";
+		yield return (object)new WaitForSeconds(3f);
+		ResourceRequest resourceRequest = Resources.LoadAsync(url);
+		yield return (object)resourceRequest;
+		GameObject prefab = resourceRequest.asset as GameObject;
+		Singleton<PreloadSystem>.Instance.cacheGameObject(prefab, url);
+	}
+
+	private void playMovies()
+	{
+		loadMovieList();
+		m_showVideo = true;
+		playMovieTexture(m_movieIndex);
+	}
+
+	private void OnGUI()
+	{
+		if (m_showVideo)
+		{
+			if (!m_movieList[m_movieIndex].isPlaying)
+			{
+				if (m_movieIndex == m_movieList.Count - 1)
+				{
+					m_showVideo = false;
+					onMovieFinished();
+					return;
+				}
+				m_movieIndex++;
+				playMovieTexture(m_movieIndex);
+			}
+			GUI.DrawTexture(new Rect(0f, 0f, (float)Screen.width, (float)Screen.height), m_movieList[m_movieIndex], ScaleMode.StretchToFill, false, 0f);
+		}
+	}
+
+	private void loadMovieList()
+	{
+		string[] movieList = MovieList;
+		foreach (string str in movieList)
+		{
+			MovieTexture item = Resources.Load("Video_PCX1/" + str) as MovieTexture;
+			m_movieList.Add(item);
+		}
+	}
+
+	private void playMovieTexture(int _idx)
+	{
+		m_movieList[_idx].Play();
+		AudioSource component = base.GetComponent<AudioSource>();
+		component.clip = m_movieList[_idx].audioClip;
+		component.Play();
+		if (_idx == 0)
+		{
+			SimpleSingleton<AudioManager>.Instance.playEvent("Play_Sfx_UNO_Logo");
+		}
+	}
+
+	private string GenerateMoviePath(string _str)
+	{
+		return Path.Combine(Application.streamingAssetsPath, "Movies//intro//" + _str + ".mp4");
+	}
+
+	private void onMovieFinished()
+	{
+		base.StartCoroutine(end());
+	}
+
+	private IEnumerator end()
+	{
+		m_epilepsyWarning.GetComponent<epliepsyController>().ShowVideoWarn();
+		yield return (object)new WaitForSeconds(5f);
+		SceneManager.LoadScene("TitleScreen", LoadSceneMode.Single);
+	}
+
+	private void initGamePlayLogic()
+	{
+		RootScript.RunInBackground(false, "SplashScreen.Start");
+		SimpleSingleton<MultiplayerManager>.Instance.Attach();
+		SimpleSingleton<ProductManager>.Instance.Attach();
+		SimpleSingleton<UserManagerAdapter>.Instance.Attach();
+		SimpleSingleton<AudioManager>.Instance.Attach();
+		SimpleSingleton<AudioManager>.Instance.registerSoundBank();
+	}
+}
